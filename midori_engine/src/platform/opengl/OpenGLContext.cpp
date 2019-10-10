a9589c53ad810438cf97f13bd2bbb7b604a429ee
SzyJ <szy.jackiewicz@gmail.com>
2019-10-10 21:20:20 +0100

    Add graphics debug info on start

--- midori_engine/src/platform/opengl/OpenGLContext.cpp
+++ midori_engine/src/platform/opengl/OpenGLContext.cpp
@@ -13,7 +13,9 @@ namespace midori {
     void OpenGLContext::Init() {
         glfwMakeContextCurrent(m_WindowHandle);
         int gladInitSuccess = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
-        MD_ASSERT(gladInitSuccess, "Failed to initialise Glad");
+        MD_CORE_ASSERT(gladInitSuccess, "Failed to initialise Glad");
+
+        MD_CORE_INFO("OpenGL Info:\n\tVendor: {0}\n\tRenderer: {1}\n\tVersion: {2}", glGetString(GL_VENDOR), glGetString(GL_RENDERER), glGetString(GL_VERSION));
     }
 
     void OpenGLContext::SwapBuffers() {
