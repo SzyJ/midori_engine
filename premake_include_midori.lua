1a34d08dd4a0c65eb67fb2bc5614731ea85b45fa
Work Account <szy.jackiewicz@gmail.com>
2019-11-21 20:35:02 +0000

    Fix include directories not being applied correclty when including library

--- premake_include_midori.lua
+++ premake_include_midori.lua
@@ -4,17 +4,19 @@
 -- File: premake_include_midori.lua
 -- Date: 21/11/2019
 
+-- Requires midori_path to be configured to the location of project folder relative to workspace location
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Midori Dependencies
 midoriincludes = {}
-midoriincludes["spdlog"]        = "midori_engine/3rd_party/spdlog/include"
-midoriincludes["glfw"]          = "midori_engine/3rd_party/glfw/include"
-midoriincludes["glad"]          = "midori_engine/3rd_party/glad/include"
-midoriincludes["imgui"]         = "midori_engine/3rd_party/imgui"
-midoriincludes["glm"]           = "midori_engine/3rd_party/glm"
-midoriincludes["stb_image"]     = "midori_engine/3rd_party/stb_image"
-midoriincludes["tinyobjloader"] = "midori_engine/3rd_party/tinyobjloader"
+midoriincludes["spdlog"]        = ("%{wks.location}/" .. midori_path .. "midori_engine/3rd_party/spdlog/include")
+midoriincludes["glfw"]          = ("%{wks.location}/" .. midori_path .. "midori_engine/3rd_party/glfw/include")
+midoriincludes["glad"]          = ("%{wks.location}/" .. midori_path .. "midori_engine/3rd_party/glad/include")
+midoriincludes["imgui"]         = ("%{wks.location}/" .. midori_path .. "midori_engine/3rd_party/imgui")
+midoriincludes["glm"]           = ("%{wks.location}/" .. midori_path .. "midori_engine/3rd_party/glm")
+midoriincludes["stb_image"]     = ("%{wks.location}/" .. midori_path .. "midori_engine/3rd_party/stb_image")
+midoriincludes["tinyobjloader"] = ("%{wks.location}/" .. midori_path .. "midori_engine/3rd_party/tinyobjloader")
 
 -- 3rd Party Projects --
 include "premake_projects/glfw.lua"
